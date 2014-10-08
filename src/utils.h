7ddda7fe0b4530ce645e5d35d5d37ac7de3b5211
Trey Marc <trey.marc@gmail.com>
2014-10-08 21:06:07 +0200

    Update utils.h
    see comment in the code :)

--- src/utils.h
+++ src/utils.h
@@ -4,3 +4,6 @@ int constrain(int amt, int low, int high);
 // sensor orientation
 void alignSensors(int16_t *src, int16_t *dest, uint8_t rotation);
 void initBoardAlignment(void);
+
+
+// hi , we have a dev meeting in paris , semd me a pm or email me asap :)  #multiwii approved
